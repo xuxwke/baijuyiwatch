fbcb3efebd6a00ebd01c591998c7808149581f1a
hy <hy@qq.com>
2025-07-21 22:18:21 +0800

    add macos

--- ui_app/macos/Runner/MainFlutterWindow.swift
+++ ui_app/macos/Runner/MainFlutterWindow.swift
@@ -1,15 +1,25 @@
 import Cocoa
 import FlutterMacOS
 
-class MainFlutterWindow: NSWindow {
+class MainFlutterWindow: NSPanel {
+  var isReadyToShow: Bool = false
+  
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.setFrame(windowFrame, display: false)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
+  }
+
+  override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
+    super.order(place, relativeTo: otherWin)
+    
+    if !isReadyToShow {
+      setIsVisible(false)
+    }
   }
 }
